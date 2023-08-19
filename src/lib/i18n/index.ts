91c94b8e79a58202f67b83fbd5a8633a400ab6c8
arminmiau <arminmiau.dev@gmail.com>
2023-08-19 10:06:39 +0200

    switch to svelte-kit

---
+++ src/lib/i18n/index.ts
@@ -0,0 +1,31 @@
+import { browser } from '$app/environment';
+import { register, init, waitLocale, locale } from 'svelte-i18n';
+
+const defaultLocale = 'en-GB';
+
+register('en-GB', () => import('$lib/i18n/locales/en.json'));
+register('de-DE', () => import('$lib/i18n/locales/de.json'));
+
+let localeCookie: string | undefined = undefined;
+if (browser) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; locale=`);
+    if (parts.length === 2)
+        localeCookie = parts.pop()?.split(';').shift() ?? undefined;
+}
+
+const initialLocale = localeCookie
+    ? localeCookie
+    : browser
+    ? window.navigator.language
+    : defaultLocale;
+init({
+    fallbackLocale: defaultLocale,
+    initialLocale: initialLocale,
+});
+
+await waitLocale();
+
+locale.subscribe((locale) => {
+    if (browser && locale) document.cookie = 'locale=' + locale;
+});
